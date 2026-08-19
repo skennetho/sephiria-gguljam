44be150cea034b61db8caafe4eccfec18e1a103b
skennetho <thrkd613@naver.com>
2026-08-20 00:07:42 +0900

    chore(release): bump version to 0.2.0

--- SephiriaPlugin/Plugin.cs
+++ SephiriaPlugin/Plugin.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 namespace SephiriaTools
 {
-    [BepInPlugin("com.sephiria.tools", "SephiriaTools", "1.0.0")]
+    [BepInPlugin("com.sephiria.tools", "SephiriaTools", "0.2.0")]
     public class Plugin : BaseUnityPlugin
     {
         public static Plugin Instance { get; private set; }
